f3aad7d5c60c7260d33d16ee19b709a82217ad2b
Henry Pérez García <henry.perez@agrosoft.com.ec>
2023-01-29 15:24:03 -0600

    ✨ SideNavBar Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+interface SideNavToggle {
+  screenWidth: number,
+  collapsed: boolean
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'InsuranceAPP';
+  isSideNavCollapsed = false
+  screenWidth =0
+
+  onToggleSideNav(data:SideNavToggle){
+    this.screenWidth = data.screenWidth
+    this.isSideNavCollapsed = data.collapsed
+  }
+}
